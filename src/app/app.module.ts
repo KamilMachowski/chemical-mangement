445ab2c2d08be38153d062ef4612bb85bd49f20b
KamilMachowski <45885548+KamilMachowski@users.noreply.github.com>
2019-12-18 16:03:53 +0100

    adding main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { SuccessComponent } from './success/success.component';
+import { ReadComponent } from './read/read.component';
+import { UpdateComponent } from './update/update.component';
+import { CreateComponent } from './create/create.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MainComponent,
+    LoginComponent,
+    SuccessComponent,
+    ReadComponent,
+    UpdateComponent,
+    CreateComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
