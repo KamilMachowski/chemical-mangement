675ad9c378302561744bfa02aca66e7587543868
KamilMachowski <45885548+KamilMachowski@users.noreply.github.com>
2019-12-19 11:14:52 +0100

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
+import { SuccessComponent } from './success/success.component';
+import { LoginComponent } from './login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'read', component: ReadComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'delete', component: DeleteComponent },
+  { path: 'success', component: SuccessComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
