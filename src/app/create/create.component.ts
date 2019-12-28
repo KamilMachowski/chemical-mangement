7fd65c0edb3be69ca3b4be6e3bdaa52d3b2ddc31
KamilMachowski <45885548+KamilMachowski@users.noreply.github.com>
2019-12-29 00:38:31 +0100

    fully working item adding

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Item } from '../_models/item';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  @Input() item: Item = new Item();
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  create(){
+    const data = JSON.stringify(this.item);
+    // console.log(data);
+    this.apiService.postData(data).subscribe(res => console.log(res));
   }
-
 }
